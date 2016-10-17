6adc3b86e8615678b917e6cfd022caef508b7674
nhaglind <nhaglind@gmail.com>
2016-10-17 15:29:27 -0500

    updated visuals and make request in one line

--- mybot.js
+++ mybot.js
@@ -6,36 +6,28 @@ var bot = new Discord.Client();
 
 bot.on("message", msg => {
 
-  // prefix var
-  let prefix = "/";
-  // prefix check
-  if(!msg.content.startsWith(prefix)) return;
-  // bots don't talk to themselves (anti-skynet provision)
-  if(msg.author.bot) return;
-  if (msg.content.startsWith(prefix + "define")) {
-    console.log(msg.content);
-
-    var url = 'https://owlbot.info/api/v1/dictionary/' + msg.content.slice(8);
-    console.log(url);
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var definitionData = JSON.parse(body);
-        msg.channel.sendMessage("Type: " + definitionData[0].type);
-        msg.channel.sendMessage("Defenition: " + definitionData[0].defenition);
-        msg.channel.sendMessage("Example: " + definitionData[0].example);
-      }
-    })
-  }
-
-  else if (msg.content.startsWith(prefix + "buffy")) {
-    msg.channel.sendMessage("I may be dead, but I’m still pretty.");
-  }
+    let prefix = "/";
+    if (!msg.content.startsWith(prefix)) return;
+    // bots don't talk to themselves (anti-skynet provision)
+    if (msg.author.bot) return;
+    if (msg.content.startsWith(prefix + "define")) {
+        console.log(msg.content);
+        var url = 'https://owlbot.info/api/v1/dictionary/' + msg.content.slice(8);
+        console.log(url);
+        request(url, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var DefinitionData = JSON.parse(body);
+                msg.channel.sendMessage("**Definition: ** " + "```" + DefinitionData[0].type + ": " + DefinitionData[0].defenition + "```" + "\n" +
+                    "**Example: ** " + "```" + DefinitionData[0].example + "```");
+            }
+        })
+    } else if (msg.content.startsWith(prefix + "buffy")) {
+        msg.channel.sendMessage("I may be dead, but I’m still pretty.");
+    }
 });
 
 bot.on('ready', () => {
     console.log('I am ready!');
 });
-
-// console.log(secrets.discordSecret());
 
 bot.login(secrets.discordSecret());
